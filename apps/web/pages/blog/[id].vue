2b9b1f99e4f32fe6e3e134656ec9b048c085411b
2005balaji <balajisr000@gmail.com>
2023-06-24 23:28:52 +0530

    changes folder structure for ease of development

--- apps/web/pages/blog/[id].vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="text-blue-800">
-        <h1>http://localhost:3000/blog/{{ route.params.id }}</h1>
-    </div>
-</template>
-
-<script setup >
-const route = useRoute()
-</script>
--- apps/web/pages/blog/create.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <h1>http://localhost:3000/blog/create</h1>
-    </div>
-</template>
-
-<script setup >
-const route = useRoute()
-</script>
