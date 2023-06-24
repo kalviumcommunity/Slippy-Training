9b8620af72fd7c1c88dd4b4c7e96b8bc6ee0abce
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-06-24 21:43:58 +0530

    File Structure

---
+++ apps/web/pages/blog/create.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>http://localhost:3000/blog/create</h1>
+    </div>
+</template>
+
+<script setup >
+const route = useRoute()
+</script>
---
+++ apps/web/pages/blogs/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>http://localhost:3000/blogs</h1>
+    </div>
+</template>
+
+<script setup >
+
+</script>
+
