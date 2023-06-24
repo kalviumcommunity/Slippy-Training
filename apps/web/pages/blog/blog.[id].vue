aae8e551b0a7835be329e52e7d017d67bdf31d65
2005balaji <balajisr000@gmail.com>
2023-06-24 23:38:20 +0530

    added in template files to fill in code

---
+++ apps/web/pages/blog/blog.[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>This is where blogs will be read</h1>
+    <p>on -clicking the blog , it should redirect with unique id</p>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped></style>
