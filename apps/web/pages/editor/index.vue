ee33aed1e8d6712650b32e03f1c301576a2ae58f
2005balaji <balajisr000@gmail.com>
2023-06-24 23:26:57 +0530

    added editor folder to setup quill

---
+++ apps/web/pages/editor/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+        <h1>Quill Editor</h1>
+            <div>
+                    <p>
+                        this is where quill editor goes in
+                    </p>
+            </div>
+
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
