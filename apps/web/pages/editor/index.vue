581ac3dd4095dffae1b6dbd23fa9445d0547eff7
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-06-25 11:58:03 +0530

    changed index.vue in editor

--- apps/web/pages/editor/index.vue
+++ apps/web/pages/editor/index.vue
@@ -12,16 +12,10 @@
 
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 
-export default defineComponent({
-    setup () {
-        
 
-        return {}
-    }
-})
+
 </script>
 
 <style scoped>
