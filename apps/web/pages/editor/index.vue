3222c3e64e7dac84b09e12b5e3b344b9096a9ff3
2005balaji <balajisr000@gmail.com>
2023-06-25 20:06:12 +0530

    fixed meage conflicts

--- apps/web/pages/editor/index.vue
+++ apps/web/pages/editor/index.vue
@@ -1,28 +1,3 @@
-<!-- <template>
-    <div>
-
-        <h1>Quill Editor</h1>
-            <div>
-                    <p>
-                        this is where quill editor goes in
-                    </p>
-            </div>
-
-    </div>
-
-</template>
-
-<script setup lang="ts">
-
-
-
-</script>
-
-<style scoped>
-
-</style> -->
-
-
 <script setup>
 import { QuillEditor } from '@vueup/vue-quill';
 import BlotFormatter from 'quill-blot-formatter'
