fea922ce48e0259bfba28032eaa6357a2cd49e34
balaji <balaji@pop-os.localdomain>
2023-06-26 19:26:28 +0530

    styled quill editor and added more edit features

--- apps/web/pages/editor/index.vue
+++ apps/web/pages/editor/index.vue
@@ -1,14 +1,18 @@
 <template>
-    <h1>This is VueQuill</h1>
-    <QuillEditor theme="snow" :toolbar="toolbar" :modules="modules" v-model:content="editorContent" content-type="html" />
-    <br>
+
+            <div>
+                    <h2 class="h-8 flex items-center justify-center">Edit with Quill</h2>
+            </div>
+    <div class="bg-gray-200 h-screen mx-8 mr-96 ">
+        <QuillEditor class="" theme="snow" :toolbar="toolbar" :modules="modules" v-model:content="editorContent"
+            content-type="html" />
+    </div>
     <button @click="showContent">Preview Content</button>
     <br>
     <div id="content_container" class="ql-container ql-snow">
         <div id="content" class="ql-editor" v-html="contentHTML"></div>
     </div>
 </template>
-
 
 <script setup>
 import { QuillEditor } from '@vueup/vue-quill';
@@ -17,11 +21,17 @@ import ImageUploader from 'quill-image-uploader';
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import 'quill-image-uploader/dist/quill.imageUploader.min.css';
 
-
 const toolbar = [
     [{ header: [1, 2, false] }],
-    ['bold', 'italic', 'underline'],
-    ['image', 'code-block']
+    ['bold', 'italic', 'underline', 'strike', 'underline', 'link'],
+    ['image', 'video',],
+    ['color', 'script',]
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+
 ];
 
 const modules = [
