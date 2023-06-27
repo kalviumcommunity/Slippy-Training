65283faf0b55d7324778ffa3ebc6e0a80539328c
Ranapriyanshi <112609820+Ranapriyanshi@users.noreply.github.com>
2023-06-27 16:41:47 +0530

    Removed Normal CSS

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -2,18 +2,18 @@
     <div>
         <div class="bg-black text-white flex flex-col">
             <div class="h-150 flex">
-                <h1 class="w-1/2 text-xl h-200">Stories that help you Learn</h1>
+                <h1 class="w-1/2 text-8xl h-200 font-semibold">Stories that help you Learn</h1>
                 <div class="w-1/2 mt-50 flex">
                     <img :src="people" alt="" class="w-2/3 ml-150 mt-150">
                 </div>
             </div>
             <div>
-                <input type="text" class="h-20 w-1/5 rounded-lg">
-                <button class="bg-orange-500 text-black h-20 rounded-lg px-4 m-15">Subscribe Now</button>
+                <input type="text" class="h-10 w-1/5 rounded-full text-black">
+                <button class="bg-orange-500 text-black h-10 rounded-full px-4 m-15">Subscribe Now</button>
             </div>
             <div class="flex flex-col items-center justify-center">
                 <img :src="elements" alt="">
-                <p class="text-l">Got quality content reading on every topic. Turn your blog reading into a magnificent experience with <span>Blogify</span></p>
+                <p class="text-3xl">Got quality content reading on every topic. Turn your blog reading into a magnificent experience with <span>Blogify</span></p>
 
             </div>
             
@@ -21,8 +21,8 @@
             <div class="flex items-center justify-center">
                 <img :src="blogger" alt="">
                 <div class="flex items-center justify-around flex-col w-1/3">
-                    <span class="text-l">Amplify Your voice, Illuminate the World: Blogging made effortless, Start your journey as blogger today with us</span>
-                    <button class="bg-orange-500 text-black h-20 rounded-lg px-4 m-15">Start Blogging Now</button>
+                    <span class="text-3xl">Amplify Your voice, Illuminate the World: Blogging made effortless, Start your journey as blogger today with us</span>
+                    <button class="bg-orange-500 text-black h-10 rounded-full px-4 m-15">Start Blogging Now</button>
                 </div>
             </div>
             <hr>
@@ -36,7 +36,7 @@
                 <BlogMyBlogs v-for="blog in blogs" :key="blog.id" :blog="blog"/>
             </div>
             <div class="flex align-center justify-center">
-                <button class="bg-orange-500 text-black h-20 rounded-lg px-4 m-15 w-1/10">Continue Reading</button>
+                <button class="bg-orange-500 text-black h-10 rounded-full px-4 m-15 w-1/10">Continue Reading</button>
             </div>
             
 
@@ -55,21 +55,5 @@ import politics from '../assests/images/politics.png'
 </script>
 
 <style>
-    .text-xl{
-        font-size: 5.5rem;
-        line-height:1;
-        font-weight: bolder;
-    }
-    .text-l{
-        font-size: 1.5rem;
-        line-height:1;
-        font-weight: bolder;
-    }
-    .h-20{
-        height: 6vh;
-    }
-    .rounded-lg{
-        border-radius: 2rem;
-    }
 </style>
 
