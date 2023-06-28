acca6a608435c5aa973b1ff978d0e0dca4d635a9
2005balaji <balajisr000@gmail.com>
2023-06-28 16:36:20 +0530

    added test get request

--- apps/web/server/api/blogs.get.ts
+++ apps/web/server/api/blogs.get.ts
@@ -1,3 +1,8 @@
-export default defineEventHandler(async(event)=>{
-    return "Getting all Blogs!! with /api/blogs"
+import { firestore } from '../utils/firebase';
+
+export default defineEventHandler(async event => { 
+    const ref = firestore.doc(`testdb/testdb`);
+    const snapshot = await ref.get();
+    const data = snapshot.data();
+    return (data);
 })
