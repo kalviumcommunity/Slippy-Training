d36f5ee5a15a48d9d60b9373285bc74ed0a2d1ff
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-06-29 23:25:06 +0530

    Added .env and added it in nect.config.ts

--- apps/web/server/utils/firebase.ts
+++ apps/web/server/utils/firebase.ts
@@ -1,11 +1,12 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+const config = useRuntimeConfig()
 export const app = initializeApp({
   credential: cert({
-    projectId: "",
-    clientEmail: "",
-    privateKey: "",
+    projectId: config.projectId,
+    clientEmail: config.clientEmail,
+    privateKey: config.privateKey,
   }),
 });
 export const firestore = getFirestore();
